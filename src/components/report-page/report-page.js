7f453d96454c9814c6e11dfc626311c5ba033010
Noah Talerman <noahtal@umich.edu>
2020-04-13 00:12:39 -0400

    individual project page progress

---
+++ src/components/report-page/report-page.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import reportPageStyles from './report-page.module.scss'
+import BannerImgWellnest from '../../images/banner.png'
+
+const Title = (props) => {
+    return (
+        <h3 className={reportPageStyles.title}>{props.text}</h3>
+    )
+}
+
+const BannerImg = (props) => {
+    return (
+        <div>
+            <img src={props.imgSrc} alt={props.imgAlt} />
+        </div>
+    );
+}
+
+const Header = (props) => {
+    return (
+        <h4 className={reportPageStyles.header}>{props.text}</h4>
+    )
+}
+
+const Body = (props) => {
+    return (
+        <p className={reportPageStyles.body}>{props.text}</p>
+    )
+}
+
+const ReportPage = () => {
+    const project = {
+        title: 'Wellnest Marketing Site v2',
+        bannerImg: 'banner-img.png',
+        sections: [
+            {
+                header: 'Problem',
+                body: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mi eget suspendisse cursus eros, egestas sit ut. Sed velit blandit cursus lorem semper in. Nunc consectetur a at lectus pharetra sed massa aenean. Feugiat ac sapien sed nullam. Imperdiet pretium convallis aliquam a nisl sagittis risus. Id gravida dictum et blandit posuere viverra massa laoreet. Odio aliquet arcu interdum maecenas. Nisl risus neque, massa orci amet, at senectus feugiat sit. Etiam donec aliquam integer iaculis imperdiet amet, id fringilla. Mauris nibh cursus varius molestie. Amet, ipsum nibh varius imperdiet.']
+            },
+            {
+                header: 'Solution',
+                body: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mi eget suspendisse cursus eros, egestas sit ut. Sed velit blandit cursus lorem semper in. Nunc consectetur a at lectus pharetra sed massa aenean. Feugiat ac sapien sed nullam. Imperdiet pretium convallis aliquam a nisl sagittis risus. Id gravida dictum et blandit posuere viverra massa laoreet. Odio aliquet arcu interdum maecenas. Nisl risus neque, massa orci amet, at senectus feugiat sit. Etiam donec aliquam integer iaculis imperdiet amet, id fringilla. Mauris nibh cursus varius molestie. Amet, ipsum nibh varius imperdiet.']
+            },
+            {
+                header: 'Something',
+                body: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mi eget suspendisse cursus eros, egestas sit ut. Sed velit blandit cursus lorem semper in. Nunc consectetur a at lectus pharetra sed massa aenean. Feugiat ac sapien sed nullam. Imperdiet pretium convallis aliquam a nisl sagittis risus. Id gravida dictum et blandit posuere viverra massa laoreet. Odio aliquet arcu interdum maecenas. Nisl risus neque, massa orci amet, at senectus feugiat sit. Etiam donec aliquam integer iaculis imperdiet amet, id fringilla. Mauris nibh cursus varius molestie. Amet, ipsum nibh varius imperdiet.']
+            }
+        ],
+
+    };
+
+    const sections = Object.keys(project.sections).map((key, i) => {
+        const bodies = project.sections[key].body.map((paragraph, i) => {
+            return (
+                <Body className={reportPageStyles.body} text={paragraph} />
+            )
+        })
+
+        console.log(bodies)
+
+        return (
+            <section className={reportPageStyles.section}>
+                <Header text={project.sections[key].header} />
+                {bodies}
+            </section>
+        );
+    })
+
+    return (
+        <>
+            <Title text='Project Title' />
+            <BannerImg imgSrc={BannerImgWellnest} imgAlt='alt' />
+            {sections}
+        </>
+    );
+}
+
+export default ReportPage
--- src/pages/projects/project-1.js
+++ src/pages/projects/project-1.js
@@ -3,7 +3,7 @@ import IntroSection from '../../components/intro-section/intro-section.js'
 import Layout from '../../components/layout/layout.js'
 import Main from '../../components/main/main.js'
 import Article from '../../components/article/article.js'
-import ResumePage from '../../components/resume-page/resume-page.js'
+import ReportPage from '../../components/report-page/report-page.js'
 import Button from '../../components/button/button.js'
 
 const Page = () => {
@@ -13,7 +13,7 @@ const Page = () => {
             <Main location="Projects wellnest-v2">
                 <Button url="/projects" content="<" arrow="arrow" left="left" />
                 <Article>
-                    <ResumePage />
+                    <ReportPage />
                 </Article>
             </Main>
         </Layout>
